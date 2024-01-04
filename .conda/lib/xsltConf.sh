4621e65c173cf38c764447b486b345168fc31da5
paul-s-prk <paul.park1@hotmail.com>
2024-01-05 06:23:48 +1100

    first commit

---
+++ .conda/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/sungpark/Code/mma-webscraping-eda/.conda/lib"
+XSLT_LIBS="-lxslt -L/Users/sungpark/Code/mma-webscraping-eda/.conda/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/sungpark/Code/mma-webscraping-eda/.conda/include"
+MODULE_VERSION="xslt-1.1.37"
---
+++ README.md
@@ -0,0 +1,1 @@
+# mma-webscraping-eda
